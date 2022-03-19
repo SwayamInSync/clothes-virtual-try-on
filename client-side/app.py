2bebc7955603846b5ddf72942691d607a219e975
practice404 <hawkempire007@gmail.com>
2022-03-20 00:07:15 +0530

    client side

---
+++ client-side/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify, render_template
+from PIL import Image
+import requests
+from io import BytesIO
+import base64
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route("/preds", methods=['POST'])
+def submit():
+    cloth = request.files['cloth']
+    model = request.files['model']
+
+    url = "http://21fb-34-77-44-39.ngrok.io/api/transform"
+    print("sending")
+    response = requests.post(url=url, files={"cloth":cloth.stream, "model":model.stream})
+    op = Image.open(BytesIO(response.content))
+
+    buffer = BytesIO()
+    op.save(buffer, 'png')
+    buffer.seek(0)
+
+    data = buffer.read()
+    data = base64.b64encode(data).decode()
+
+
+    return render_template('index.html', op=data)
+    # return render_template('index.html', test=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
