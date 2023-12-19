5371c5f5f1c7cc3340c76bb843c960f664151f33
practice404 <hawkempire007@gmail.com>
2023-12-19 20:12:46 +0530

    remove rem.bg dependency

--- client-side/app.py
+++ client-side/app.py
@@ -18,7 +18,7 @@ def submit():
     model = request.files['model']
 
     ## replace the url from the ngrok url provided on the notebook on server.
-    url = "http://b89a-34-79-184-67.ngrok.io/api/transform"
+    url = "http://e793-34-123-73-186.ngrok-free.app/api/transform"
     print("sending")
     response = requests.post(url=url, files={"cloth":cloth.stream, "model":model.stream})
     op = Image.open(BytesIO(response.content))
--- remove_bg.py
+++ remove_bg.py
@@ -2,6 +2,7 @@ import requests
 import os
 from PIL import Image
 import numpy as np
+from rembg import remove
 
 
 class preprcessInput:
@@ -16,15 +17,8 @@ class preprcessInput:
         self.t_image = None
         self.save_path = None
 
-    def remove_bg(self, file_path:str, api_key='qYVjUusWpt9ew8AUXgAEdsxv'):
+    def remove_bg(self, file_path: str):
         self.save_path = file_path[:-3]+'.png'
-        response = requests.post(
-            'https://api.remove.bg/v1.0/removebg',
-            files={'image_file': open(file_path, 'rb')},
-            data={'size': 'auto'},
-            headers={'X-Api-Key': api_key},
-        )
-
         pic = Image.open(file_path)
         self.o_width = np.asarray(pic).shape[1]
         self.o_height = np.asarray(pic).shape[0]
@@ -33,16 +27,9 @@ class preprcessInput:
         except Exception as e:
             print("Single channel image and error", e)
         os.remove(file_path)
-        if response.status_code == requests.codes.ok:
-            with open(self.save_path, 'wb') as out:
-                out.write(response.content)
-        else:
-            # raise("Error:", response.status_code, response.text)
-            print(response.status_code)
-
-        self.o_image = Image.open(self.save_path)
+        self.o_image = remove(pic)
+        self.o_image.save(self.save_path)
         os.remove(self.save_path)
-        # .convert('RGB')
         return np.asarray(self.o_image)
 
     def transform(self, width=768, height=1024):
@@ -66,8 +53,7 @@ class preprcessInput:
 # USAGE OF THE CLASS
 preprocess = preprcessInput()
 for images in os.listdir('/content/inputs/test/image'):
-  print(images)
-  if images[-3:] == 'jpg':
-    print('yo')
-    op = preprocess.remove_bg(r'/content/inputs/test/image/'+images)
-    arr = preprocess.transform(768, 1024)
+    print(images)
+    if images[-3:] == 'jpg':
+        op = preprocess.remove_bg(r'/content/inputs/test/image/'+images)
+        arr = preprocess.transform(768, 1024)
